4bcadf8ca11b1aef482f2d8eeb00a23cf0fd1b6c
Garami Gábor <hron@hron.me>
2014-05-22 12:30:07 +0200

    Adding Easter calculation for HolidayHelper
    Calculating Easter Sunday and Monday.

--- src/main/java/cofh/util/HolidayHelper.java
+++ src/main/java/cofh/util/HolidayHelper.java
@@ -1,6 +1,7 @@
 package cofh.util;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * The class contains helper functions related to Holidays!
@@ -73,8 +74,53 @@ public class HolidayHelper {
 	}
 
 	public static boolean isEaster() {
+		/*
+		 * Compute the day of the year that Easter falls on. Step names E1 E2 etc.,
+		 * are direct references to Knuth, Vol 1, p 155.
+		 *
+		 * http://en.wikipedia.org/wiki/Computus#Meeus.2FJones.2FButcher_Gregorian_algorithm
+		 */
 
-		return false;
+		Calendar easterSun;
+
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		if(year <= 1582) {
+			return false; // The calculation is based on Gregorian calendar and it's incorrect before 1582
+		}
+
+		int golden, century, x, z, d, epact, n;
+
+		golden = (year % 19) + 1; /*  metonic cycle */
+
+		century = (year / 100) + 1; /* Centuries are shifted by one e.g. 1984 was in 20th C */
+
+		x = (3 * century / 4) - 12; /* leap year correction */
+		z = ((8 * century + 5) / 25) - 5; /* syncing with moon's orbit */
+
+		d = (5 * year / 4) - x - 10;
+
+		epact = (11 * golden + 20 + z - x) % 30; /* epact */
+
+		if ((epact == 25 && golden > 11) || epact == 24) {
+			epact++;
+		}
+
+		n = 44 - epact;
+		n += 30 * (n < 21 ? 1 : 0);
+		n += 7 - ((d + n) % 7);
+
+		if (n > 31) {
+			easterSun = new GregorianCalendar(year, 4 - 1, n - 31); /* if April */
+		} else {
+			easterSun = new GregorianCalendar(year, 3 - 1, n); /* if March */
+		}
+
+		setDate(holidayStart, easterSun.get(Calendar.MONTH), easterSun.get(Calendar.DAY_OF_MONTH));
+		setDate(holidayEnd, easterSun.get(Calendar.MONTH), easterSun.get(Calendar.DAY_OF_MONTH) + 1);
+
+		curTime = Calendar.getInstance();
+
+		return curTime.after(holidayStart) && curTime.before(holidayEnd);
 	}
 
 	public static boolean isUSIndependenceDay() {
