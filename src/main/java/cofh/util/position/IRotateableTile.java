35afbd2e23ef0c71bb4a738bbdb0ad4c9ae77818
skyboy <skyboy026@gmail.com>
2014-06-15 09:35:04 -0400

    Add a couple methods to IRotateableTile

--- src/main/java/cofh/util/position/IRotateableTile.java
+++ src/main/java/cofh/util/position/IRotateableTile.java
@@ -4,9 +4,13 @@ import net.minecraftforge.common.util.ForgeDirection;
 
 public interface IRotateableTile {
 
+	public boolean canRotate();
+	
 	public boolean canRotate(ForgeDirection axis);
 
 	public void rotate(ForgeDirection axis);
+	
+	public void rotateDirectlyTo(int facing);
 
 	public ForgeDirection getDirectionFacing();
 
