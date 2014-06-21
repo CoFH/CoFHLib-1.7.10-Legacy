cb0f152246f509033b5807cf95cd78efb029de7a
ZeldoKavira <zeldokavira@gmail.com>
2014-06-21 17:28:05 -0400

    IPlacedTile
    Signed-off-by: ZeldoKavira <zeldokavira@gmail.com>

---
+++ src/main/java/cofh/api/tileentity/IPlacedTile.java
@@ -0,0 +1,12 @@
+package cofh.api.tileentity;
+
+/**
+ * Implement this interface on Tile Entities which need to know when they are placed into the world.
+ * 
+ * @author Zeldo
+ * 
+ */
+public interface IPlacedTile {
+
+	public void tilePlaced();
+}
