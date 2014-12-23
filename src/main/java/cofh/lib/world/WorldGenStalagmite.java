0f5d88b4daf1f2b736bfd3141507f33424b72d2b
skyboy <skyboy026@gmail.com>
2014-12-23 06:33:49 -0500

    Better control over height/size; altSinc option

--- src/main/java/cofh/lib/world/WorldGenStalagmite.java
+++ src/main/java/cofh/lib/world/WorldGenStalagmite.java
@@ -22,6 +22,7 @@ public class WorldGenStalagmite extends WorldGenerator {
 	public int genSize = 0;
 	public boolean smooth = false;
 	public boolean fat = true;
+	public boolean altSinc = false;
 
 	public WorldGenStalagmite(List<WeightedRandomBlock> resource, List<WeightedRandomBlock> block, List<WeightedRandomBlock> gblock) {
 
@@ -35,12 +36,17 @@ public class WorldGenStalagmite extends WorldGenerator {
 		if (smooth) {
 			if ((x*x + z*z) * 4 >= size * size * 5) return 0;
 
-			final double lim = (fat ? 1 : .5f) * 400f / size;
+			final double lim = (altSinc ? 600f : (fat ? 1f : .5f) * 400f) / size;
 			final double pi = Math.PI;
 			double r;
 			r = Math.sqrt((r=((x*lim)/pi))*r + (r=((z*lim)/pi))*r) * pi/180;
+			if (altSinc && (x*x + z*z) < (size*1.1f))
+				r = size * 0.0082;
 			if (r == 0) return height;
-			return (int)Math.round(height * (fat ? Math.sin(r) / r : Math.sin(r=r*pi) / r));
+			if (!altSinc)
+				return (int)Math.round(height * (fat ? Math.sin(r) / r : Math.sin(r=r*pi) / r));
+			double sinc = (Math.sin(r) / r);
+			return (int)Math.round(Math.min(sinc*height+3, height*(sinc*2+(rand.nextGaussian()*.65*Math.sin(r=r*(pi*4))/r))/2.3));
 		} else {
 			int absx = x < 0 ? -x : x, absz = (z < 0 ? -z : z);
 			int dist = fat ? (absx < absz ? absz + absx / 2 : absx + absz / 2) : absx + absz;
@@ -60,9 +66,10 @@ public class WorldGenStalagmite extends WorldGenerator {
 			return false;
 		}
 
-		int maxHeight = rand.nextInt(heightVariance) + minHeight;
+		int maxHeight = (heightVariance > 0 ? rand.nextInt(heightVariance) : 0) + minHeight;
 
-		int size = (genSize > 0 ? genSize : maxHeight / heightMod) + rand.nextInt(sizeVariance);
+		int size = (genSize > 0 ? genSize : maxHeight / heightMod);
+		if (sizeVariance > 0) size += rand.nextInt(sizeVariance);
 		boolean r = false;
 		for (int x = -size; x <= size; ++x) {
 			for (int z = -size; z <= size; ++z) {
