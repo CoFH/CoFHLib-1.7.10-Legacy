89df6b62e3f68b8cae32eb16456f88ccd0983275
Justin <holtjustin2@gmail.com>
2015-03-08 14:00:26 -0400

    Updated block classes to 1.8

--- src/main/java/cofh/api/block/IBlockDebug.java
+++ src/main/java/cofh/api/block/IBlockDebug.java
@@ -1,8 +1,8 @@
 package cofh.api.block;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.common.util.ForgeDirection;
 
 /**
  * Implement this interface on blocks which have some debug method which can be activated via a tool or other means.
@@ -28,6 +28,6 @@ public interface IBlockDebug {
 	 * @param player
 	 *            Player doing the debugging.
 	 */
-	void debugBlock(IBlockAccess world, int x, int y, int z, ForgeDirection side, EntityPlayer player);
+	void debugBlock(IBlockAccess world, int x, int y, int z, EnumFacing side, EntityPlayer player);
 
 }
--- src/main/java/cofh/api/block/IBlockInfo.java
+++ src/main/java/cofh/api/block/IBlockInfo.java
@@ -5,9 +5,9 @@ import cofh.api.tileentity.ITileInfo;
 import java.util.List;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.common.util.ForgeDirection;
 
 /**
  * Implement this interface on blocks which can provide information about themselves. If the block contains Tile Entities, then it is recommended that this
@@ -38,6 +38,6 @@ public interface IBlockInfo {
 	 * @param debug
 	 *            If true, the block should return "debug" information.
 	 */
-	void getBlockInfo(IBlockAccess world, int x, int y, int z, ForgeDirection side, EntityPlayer player, List<IChatComponent> info, boolean debug);
+	void getBlockInfo(IBlockAccess world, int x, int y, int z, EnumFacing side, EntityPlayer player, List<IChatComponent> info, boolean debug);
 
 }
