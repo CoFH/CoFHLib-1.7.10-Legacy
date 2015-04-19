e5b63abfda2651b3378c627920944a230fca26fb
skyboy <skyboy026@gmail.com>
2015-04-19 12:55:15 -0400

    Tweak equality in ItemHelper

--- src/main/java/cofh/lib/util/helpers/ItemHelper.java
+++ src/main/java/cofh/lib/util/helpers/ItemHelper.java
@@ -992,6 +992,17 @@ public final class ItemHelper {
 		return itemsEqualWithMetadata(stack, player.getCurrentEquippedItem());
 	}
 
+	/**
+	 * Determine if the damage of two ItemStacks is equal. Assumes both itemstacks are of type A.
+	 */
+	public static boolean itemsDamageEqual(ItemStack stackA, ItemStack stackB) {
+
+		return (!stackA.getHasSubtypes() && stackA.getMaxDamage() == 0) || (getItemDamage(stackA) == getItemDamage(stackB));
+	}
+
+	/**
+	 * Determine if two ItemStacks have the same Item.
+	 */
 	public static boolean itemsEqualWithoutMetadata(ItemStack stackA, ItemStack stackB) {
 
 		if (stackA == null || stackB == null) {
@@ -1000,28 +1011,42 @@ public final class ItemHelper {
 		return areItemsEqual(stackA.getItem(), stackB.getItem());
 	}
 
+	/**
+	 * Determine if two ItemStacks have the same Item and NBT.
+	 */
 	public static boolean itemsEqualWithoutMetadata(ItemStack stackA, ItemStack stackB, boolean checkNBT) {
 
 		return itemsEqualWithoutMetadata(stackA, stackB) && (!checkNBT || doNBTsMatch(stackA.stackTagCompound, stackB.stackTagCompound));
 	}
 
+	/**
+	 * Determine if two ItemStacks have the same Item and damage.
+	 */
 	public static boolean itemsEqualWithMetadata(ItemStack stackA, ItemStack stackB) {
 
-		return itemsEqualWithoutMetadata(stackA, stackB) && (getItemDamage(stackA) == getItemDamage(stackB));
+		return itemsEqualWithoutMetadata(stackA, stackB) && itemsDamageEqual(stackA, stackB);
 	}
 
+	/**
+	 * Determine if two ItemStacks have the same Item, damage, and NBT.
+	 */
 	public static boolean itemsEqualWithMetadata(ItemStack stackA, ItemStack stackB, boolean checkNBT) {
 
 		return itemsEqualWithMetadata(stackA, stackB) && (!checkNBT || doNBTsMatch(stackA.stackTagCompound, stackB.stackTagCompound));
 	}
 
-	// Functionally equivalent to itemsEqualWithMetadata(stackA, stackB, true);
+	/**
+	 * Determine if two ItemStacks have the same Item, identical damage, and NBT.
+	 */
 	public static boolean itemsIdentical(ItemStack stackA, ItemStack stackB) {
 
 		return itemsEqualWithoutMetadata(stackA, stackB) && getItemDamage(stackA) == getItemDamage(stackB)
 				&& doNBTsMatch(stackA.stackTagCompound, stackB.stackTagCompound);
 	}
 
+	/**
+	 * Determine if two NBTTagCompounds are equal.
+	 */
 	public static boolean doNBTsMatch(NBTTagCompound nbtA, NBTTagCompound nbtB) {
 
 		if (nbtA == null & nbtB == null) {
