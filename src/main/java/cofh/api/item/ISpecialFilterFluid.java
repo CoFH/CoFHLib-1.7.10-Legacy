02e0cc4bef2f1a85e6d00a1ea303bf5f84ab29d0
King Lemming <kinglemming@gmail.com>
2015-07-07 16:01:39 -0400

    Just being a jerk.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/api/item/ISpecialFilterFluid.java
+++ src/main/java/cofh/api/item/ISpecialFilterFluid.java
@@ -4,17 +4,21 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
 /**
- * Implement this interface on subclasses of Item to change how the item works in Thermal Dynamics Fluiducts filter slots
+ * Implement this interface on subclasses of Item to change how the item works in Thermal Dynamics Fluiducts filter slots.
+ *
+ * This can be used to create customizable Items which are determined to be "equal" for the purposes of filtering.
  */
 public interface ISpecialFilterFluid {
-    /**
-     * Called to find out if the given fluidstack should be matched by the given filter
-     *
-     * @param filter
-     *            ItemStack representing the filter.
-     * @param fluidstack
-     *            Fluidstack representing the fluid to match.
-     * @return True if the filter should match the fluidstack. False if the default matching should be used.
-     */
-    public boolean matchesFluid(ItemStack filter, FluidStack fluidstack);
+
+	/**
+	 * This method is called to find out if the given FluidStack should be matched by the given Filter ItemStack.
+	 *
+	 * @param filter
+	 *            ItemStack representing the filter.
+	 * @param fluid
+	 *            FluidStack representing the queried fluid.
+	 * @return True if the filter should match the FluidStack. False if the default matching should be used.
+	 */
+	public boolean matchesFluid(ItemStack filter, FluidStack fluid);
+
 }
