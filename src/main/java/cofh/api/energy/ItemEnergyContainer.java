24e200c6f2e6f75a4a3767f81c3b67a26732e37a
BrainStone <yannick@tedworld.de>
2015-12-30 09:54:02 +0100

    Allow chaining for all setters
    All setters will return the object so that multiple setters can be chained.

--- src/main/java/cofh/api/energy/ItemEnergyContainer.java
+++ src/main/java/cofh/api/energy/ItemEnergyContainer.java
@@ -43,20 +43,23 @@ public class ItemEnergyContainer extends Item implements IEnergyContainerItem {
 		return this;
 	}
 
-	public void setMaxTransfer(int maxTransfer) {
+	public ItemEnergyContainer setMaxTransfer(int maxTransfer) {
 
 		setMaxReceive(maxTransfer);
 		setMaxExtract(maxTransfer);
+		return this;
 	}
 
-	public void setMaxReceive(int maxReceive) {
+	public ItemEnergyContainer setMaxReceive(int maxReceive) {
 
 		this.maxReceive = maxReceive;
+		return this;
 	}
 
-	public void setMaxExtract(int maxExtract) {
+	public ItemEnergyContainer setMaxExtract(int maxExtract) {
 
 		this.maxExtract = maxExtract;
+		return this;
 	}
 
 	/* IEnergyContainerItem */
