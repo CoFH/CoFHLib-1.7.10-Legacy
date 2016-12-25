04e64b2b33879ed4e0a8e705206b7ad72beeffe7
covers1624 <laughlan.cov@internode.on.net>
2016-12-25 20:17:47 +1030

    Update IToolHammer to use BlockPos.

--- src/main/java/cofh/api/item/IToolHammer.java
+++ src/main/java/cofh/api/item/IToolHammer.java
@@ -2,6 +2,7 @@ package cofh.api.item;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 
 /**
  * Implement this interface on subclasses of Item to have that item work as a tool for CoFH mods.
@@ -13,22 +14,18 @@ public interface IToolHammer {
      *
      * @param item The itemstack for the tool. Not required to match equipped item (e.g., multi-tools that contain other tools)
      * @param user The entity using the tool
-     * @param x    X location of the block/tile
-     * @param y    Y location of the block/tile
-     * @param z    Z location of the block/tile
+     * @param pos  Location of the block/tile
      * @return True if this tool can be used
      */
-    boolean isUsable(ItemStack item, EntityLivingBase user, int x, int y, int z);
+    boolean isUsable(ItemStack item, EntityLivingBase user, BlockPos pos);
 
     /**
      * Callback for when the tool has been used reactively.
      *
      * @param item The ItemStack for the tool. Not required to match equipped item (e.g., multi-tools that contain other tools)
      * @param user The entity using the tool
-     * @param x    X location of the block/tile
-     * @param y    Y location of the block/tile
-     * @param z    Z location of the block/tile
+     * @param pos  Location of the block/tile
      */
-    void toolUsed(ItemStack item, EntityLivingBase user, int x, int y, int z);
+    void toolUsed(ItemStack item, EntityLivingBase user, BlockPos pos);
 
 }
