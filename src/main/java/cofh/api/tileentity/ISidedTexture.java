3c545fe339c28b3b48272d0e2c882d64d7d7af15
King Lemming <kinglemming@gmail.com>
2017-02-23 04:02:41 -0500

    Update to ISidedTexture
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/api/tileentity/ISidedTexture.java
+++ src/main/java/cofh/api/tileentity/ISidedTexture.java
@@ -5,22 +5,21 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 /**
  * Implement this interface on Tile Entities which can change their block's texture based on the current render pass. The block must defer the call to its Tile Entity.
  *
- * @author Zeldo Kavira
+ * This is only used in Configuration Tabs and blocks which do not require advanced rendering. :)
+ *
+ * @author King Lemming
  */
 public interface ISidedTexture {
 
-	int getNumLayers();
-
-	int getNumPasses(int layer);
+	int getNumPasses();
 
 	/**
 	 * Returns the icon to use for a given side and render pass.
 	 *
 	 * @param side  Block side to get the texture for.
-	 * @param layer Render layer.
-	 * @param pass  Render pass (layer specific).
+	 * @param pass  Render pass.
 	 * @return The icon to use.
 	 */
-	TextureAtlasSprite getTexture(int side, int layer, int pass);
+	TextureAtlasSprite getTexture(int side, int pass);
 
 }
