fe6c5812f2c4201e055f5435cdb50e4050515f0d
King Lemming <kinglemming@gmail.com>
2017-03-08 00:52:32 -0500

    Update version.

--- src/main/java/cofh/api/CoFHAPIProps.java
+++ src/main/java/cofh/api/CoFHAPIProps.java
@@ -6,6 +6,6 @@ public class CoFHAPIProps {
 
 	}
 
-	public static final String VERSION = "1.4.0";
+	public static final String VERSION = "1.4.1";
 
 }
--- src/main/java/cofh/lib/CoFHLibProps.java
+++ src/main/java/cofh/lib/CoFHLibProps.java
@@ -6,6 +6,6 @@ public class CoFHLibProps {
 
 	}
 
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.2.1";
 
 }
