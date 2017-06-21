698140adb80accd0043ed6ba84ca475e78f302d6
skyboy <mr.wraith.06@gmail.com>
2017-06-21 18:31:46 -0400

    Add WorldValueProvider for worldgen

---
+++ src/main/java/cofh/lib/util/numbers/world/WorldValueEnum.java
@@ -0,0 +1,70 @@
+package cofh.lib.util.numbers.world;
+
+import cofh.lib.util.helpers.BlockHelper;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+public enum WorldValueEnum {
+
+	WORLD_HEIGHT {
+		@Override
+		public long getValue(World world, Random rand, BlockPos pos) {
+
+			return world.getActualHeight();
+		}
+	},
+	SEA_LEVEL {
+		@Override
+		public long getValue(World world, Random rand, BlockPos pos) {
+
+			return world.getSeaLevel();
+		}
+	},
+	GROUND_LEVEL {
+		@Override
+		public long getValue(World world, Random rand, BlockPos pos) {
+
+			return world.provider.getAverageGroundLevel();
+		}
+	},
+	RAIN_HEIGHT {
+		@Override
+		public long getValue(World world, Random rand, BlockPos pos) {
+
+			return world.getPrecipitationHeight(pos).getY();
+		}
+	},
+	HEIGHT_MAP {
+		@Override
+		public long getValue(World world, Random rand, BlockPos pos) {
+
+			return world.getHeightmapHeight(pos.getX(), pos.getZ());
+		}
+	},
+	HIGHEST_BLOCK {
+		@Override
+		public long getValue(World world, Random rand, BlockPos pos) {
+
+			return BlockHelper.getTopBlockY(world, pos.getX(), pos.getY());
+		}
+	},
+	SURFACE_BLOCK {
+		@Override
+		public long getValue(World world, Random rand, BlockPos pos) {
+
+			return BlockHelper.getSurfaceBlockY(world, pos.getX(), pos.getZ());
+		}
+	},
+	LOWEST_CHUNK_HORIZON {
+		@Override
+		public long getValue(World world, Random rand, BlockPos pos) {
+
+			return world.getChunksLowestHorizon(pos.getX(), pos.getZ());
+		}
+	};
+
+	public abstract long getValue(World world, Random rand, BlockPos pos);
+
+}
